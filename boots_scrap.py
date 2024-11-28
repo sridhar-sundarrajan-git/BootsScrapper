cb891fe0df5ecaf4bb82f0a14dc5efeed04cdcd1
sridhar-sundarrajan-git <121281589+sridhar-sundarrajan-git@users.noreply.github.com>
2024-11-28 13:11:51 +0530

    Update boots_scrap.py

--- boots_scrap.py
+++ boots_scrap.py
@@ -1,23 +1,20 @@
 import requests
-# from bs4 import BeautifulSoup
-# from lxml import etree
+from bs4 import BeautifulSoup
+from lxml import etree
 
 # # URL of the webpage you want to scrape
 url = 'https://www.boots.com/health-pharmacy/medicines-treatments/sleep'  # Replace with your URL
 
 # # Send a GET request to fetch the page content
-# response = requests.get(url)
+response = requests.get(url)
 
 # # Check if the request was successful
-# if response.status_code == 200:
+if response.status_code == 200:
 #     # Parse the content of the page using BeautifulSoup
-#     soup = BeautifulSoup(response.text, 'html.parser')
+    soup = BeautifulSoup(response.text, 'html.parser')
 # # Find all the h3 tags with product names
-#     product_names = soup.select('#hits div.oct-teaser__contents.oct-teaser__contents--only-foreground-image .oct-teaser__contents-panel--main-content a > div > h3')
-#     print('product_names',product_names)
+    product_names = soup.select('#hits div.oct-teaser__contents.oct-teaser__contents--only-foreground-image .oct-teaser__contents-panel--main-content a > div > h3')
+    print('product_names',product_names)
 # # Loop through the list and extract the text
-#     for product in product_names:
-#         print(product.get_text(strip=True))
-
-r = requests.get(url)
-print(r.text)
+    for product in product_names:
+        print(product.get_text(strip=True))
